6f2c12a6a36593be29d6ec275a626c470b506dc5
heiljj <heiljj@rose-hulman.edu>
2025-12-13 20:07:23 -0600

    oops

--- src/usbipice/control/ControlEventSender.py
+++ src/usbipice/control/ControlEventSender.py
@@ -5,21 +5,18 @@ class ControlEventSender(EventSender):
     instructions to worker's servers.."""
     def sendDeviceReservationEnd(self, serial: str, client_id: str) -> bool:
         """Sends a reservation end event for serial."""
-        return self.sendClient(client_id, {
+        return self.sendClientJson(client_id, serial, {
             "event": "reservation end",
-            "serial": serial
         })
 
     def sendDeviceFailure(self, serial: str, client_id: str) -> bool:
         """Sends a failure event for serial."""
-        return self.sendClient(client_id, {
+        return self.sendClientJson(client_id, serial, {
             "event": "failure",
-            "serial": serial
         })
 
     def sendDeviceReservationEndingSoon(self, serial: str) -> bool:
         """Sends a reservation ending soon event for serial."""
-        return self.sendSerial(serial, {
+        return self.sendSerialJson(serial, {
             "event": "reservation ending soon",
-            "serial": serial
         })
